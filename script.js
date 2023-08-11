1e68d7c856153a0f1e87f772b3d65d984131832c
prpawlowski <p.r.pawlowski@gmail.com>
2023-08-11 17:21:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const generujButton = document.getElementById("generujButton");
+  const potwierdzButton = document.getElementById("potwierdzButton");
   const inputContainer = document.getElementById("inputContainer");
   const listaPrezentow = document.getElementById("listaPrezentow");
 
@@ -7,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const liczbaUczestnikowInput = document.getElementById("liczbaUczestnikow");
     const liczbaUczestnikow = parseInt(liczbaUczestnikowInput.value, 10);
 
-    inputContainer.innerHTML = ""; // Wyczyść pole input
+    inputContainer.innerHTML = "";
 
     for (let i = 1; i <= liczbaUczestnikow; i++) {
       const inputId = `uczestnik${i}`;
@@ -21,25 +22,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  function losujPrezenty(uczestnicy) {
-    const uczestnicyDoLosowania = [...uczestnicy];
-    const paryPrezentow = {};
-
-    for (let osoba of uczestnicy) {
-      const indeksLosowanejOsoby = Math.floor(Math.random() * uczestnicyDoLosowania.length);
-      const losowanaOsoba = uczestnicyDoLosowania.splice(indeksLosowanejOsoby, 1)[0];
-      paryPrezentow[osoba] = losowanaOsoba;
-    }
-
-    return paryPrezentow;
-  }
-
-  generujButton.addEventListener("click", () => {
-    const liczbaUczestnikowInput = document.getElementById("liczbaUczestnikow");
-    const liczbaUczestnikow = parseInt(liczbaUczestnikowInput.value, 10);
-
+  potwierdzButton.addEventListener("click", () => {
     const uczestnicy = [];
-    for (let i = 1; i <= liczbaUczestnikow; i++) {
+    for (let i = 1; i <= parseInt(liczbaUczestnikow.value, 10); i++) {
       const inputId = `uczestnik${i}`;
       const input = document.getElementById(inputId);
       if (input) {
@@ -56,4 +41,17 @@ document.addEventListener("DOMContentLoaded", () => {
       listaPrezentow.appendChild(listItem);
     }
   });
+
+  function losujPrezenty(uczestnicy) {
+    const uczestnicyDoLosowania = [...uczestnicy];
+    const paryPrezentow = {};
+
+    for (let osoba of uczestnicy) {
+      const indeksLosowanejOsoby = Math.floor(Math.random() * uczestnicyDoLosowania.length);
+      const losowanaOsoba = uczestnicyDoLosowania.splice(indeksLosowanejOsoby, 1)[0];
+      paryPrezentow[osoba] = losowanaOsoba;
+    }
+
+    return paryPrezentow;
+  }
 });
