6c082cdd5582cc3ea78eab3d14080b4c6e9d6059
prpawlowski <p.r.pawlowski@gmail.com>
2023-08-11 17:12:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const rodzina = ["Osoba1", "Osoba2", "Osoba3", "Osoba4", "Osoba5"];
-  const losujButton = document.getElementById("losujButton");
+  const generujButton = document.getElementById("generujButton");
   const listaPrezentow = document.getElementById("listaPrezentow");
 
-  function losujPrezenty(rodzina) {
-    const osobyDoLosowania = [...rodzina];
+  function losujPrezenty(uczestnicy) {
+    const uczestnicyDoLosowania = [...uczestnicy];
     const paryPrezentow = {};
 
-    for (let osoba of rodzina) {
-      const indeksLosowanejOsoby = Math.floor(Math.random() * osobyDoLosowania.length);
-      const losowanaOsoba = osobyDoLosowania.splice(indeksLosowanejOsoby, 1)[0];
+    for (let osoba of uczestnicy) {
+      const indeksLosowanejOsoby = Math.floor(Math.random() * uczestnicyDoLosowania.length);
+      const losowanaOsoba = uczestnicyDoLosowania.splice(indeksLosowanejOsoby, 1)[0];
       paryPrezentow[osoba] = losowanaOsoba;
     }
 
     return paryPrezentow;
   }
 
-  losujButton.addEventListener("click", () => {
-    const pary = losujPrezenty(rodzina);
+  generujButton.addEventListener("click", () => {
+    const liczbaUczestnikowInput = document.getElementById("liczbaUczestnikow");
+    const liczbaUczestnikow = parseInt(liczbaUczestnikowInput.value, 10);
+
+    const uczestnicy = [];
+    for (let i = 1; i <= liczbaUczestnikow; i++) {
+      const inputId = `uczestnik${i}`;
+      const input = document.getElementById(inputId);
+      if (input) {
+        uczestnicy.push(input.value);
+      }
+    }
+
+    const pary = losujPrezenty(uczestnicy);
     listaPrezentow.innerHTML = "";
 
     for (let osoba in pary) {
